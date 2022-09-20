f2702e18dce4a4d32b5c891e687e5c737e498a0a
KasaPatrik <kasaip02@gmail.com>
2022-09-20 10:17:16 +0200

    Add project files.

---
+++ WPF_teglalap/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace WPF_teglalap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            decimal a = decimal.Parse(num_a_oldal.Text);
+            decimal b = decimal.Parse(num_b_oldal.Text);
+            decimal K = 2 * a + 2 * b;
+            decimal T = a * b;
+            num_k.Text = K.ToString("0.00");
+            num_t.Text = T.ToString("0.00");
+        }
+
+
+    }
+}
